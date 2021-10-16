54bd66b0e3d15593077139b42bc2bb779be01157
jpkcunha <jpkcunha@gmail.com>
2021-10-16 12:59:06 -0300

    Teste criado

---
+++ src/Test/CasaTest.java
@@ -0,0 +1,24 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CasaTest {
+
+	@Test
+	void testSetPosX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPosY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerificaPeao() {
+		fail("Not yet implemented");
+	}
+
+}
